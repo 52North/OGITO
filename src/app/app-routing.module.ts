7b7d6059f84b928039c05dd6491be3d18db97766
Rosa Aguilar <r.m.aguilardearchila@utwente.nl>
2020-08-03 22:03:44 +0200

    adding authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {MapComponent} from './map/map.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  { path: 'map', component: MapComponent},
   {path: '**', redirectTo: ''}
 
 ];
