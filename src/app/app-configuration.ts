77db962564d9a5241d4a7422b38f86243f789cdc
Rosa Aguilar <r.m.aguilardearchila@utwente.nl>
2022-08-22 20:31:18 +0200

    Update app-configuration.ts

--- src/app/app-configuration.ts
+++ src/app/app-configuration.ts
@@ -1,23 +1,19 @@
 
 
 export class AppConfiguration{
-  // static qGsServerUrl = 'http://localhost/qgis/qgis_mapserv.fcgi.exe?';
-  // static hostname =  'http://localhost/';
   static hostname = 'https://ogito.itc.utwente.nl/';
-  // static qGsServerUrl = 'https://ogito.itc.utwente.nl/cgi-bin/qgis_mapserv.fcgi?';
   static wmsVersion  = '1.3.0' ;
   static wfsVersion = '1.1.0';
   static wmtsVersion = '1.0.0';
   static qgsProject = 'checking2.qgs';  // shapefiles
-  //static srsName = 'EPSG:32647'   // tomarlo de aqui o del projecto?
   static srsName = 'EPSG:25832';
-  // la ruta para accederlo dentro de la app
+  // Path to access projects
   static curProject =  AppConfiguration.hostname + 'qgs_projects/' + AppConfiguration.qgsProject;  // to fecth the xml project file
   static svgFolder = '../../assets/svg/';
   static svgUrl =  AppConfiguration.hostname + 'svg/';
-  static qgsProjectFolder =  '/home/qgis/projects/'//'D:/PhD/code/fromScratch/';  // 'D:/PhD/code/fromScratch/myOgito/src/assets/';
-  // la ruta para accederlo desde Qgs Server
-  static QgsFileProject = AppConfiguration.qgsProjectFolder + AppConfiguration.qgsProject; // to use with the qgis server
+  static qgsProjectFolder =  '/home/qgis/projects/'
+  // Path to use from to use with the qgis server
+  static QgsFileProject = AppConfiguration.qgsProjectFolder + AppConfiguration.qgsProject; // 
   static mapZoom = 14;
   static maxZoom = 20 ;
   static minZoom = 8 ;
