1aca239f6a83963fd3e0397dad947c0ac396c760
Arne Vogt <a.vogt@52north.org>
2022-11-07 17:10:33 +0100

    add fullscreen support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,14 @@ export class AppComponent {
   title = 'myOgito';
 
 
-
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if(event.key === 'F11'){
+      console.log("hit f11")
+      document.getElementById("app-content-container").focus()
+      document.getElementById("app-content-container").requestFullscreen()
+      event.stopPropagation()
+    }
+  }
 
 }
