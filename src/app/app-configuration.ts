abd43480d7b836b33cadbffae5d957e2ba2173d8
Arne Vogt <a.vogt@52north.org>
2022-11-30 18:08:32 +0100

    change config for local deployment

--- src/app/app-configuration.ts
+++ src/app/app-configuration.ts
@@ -1,7 +1,7 @@
 
 
 export class AppConfiguration{
-  static hostname = 'https://ogito.itc.utwente.nl/';
+  static hostname = 'http://localhost:4200/';
   static wmsVersion  = '1.3.0' ;
   static wfsVersion = '1.1.0';
   static wmtsVersion = '1.0.0';
